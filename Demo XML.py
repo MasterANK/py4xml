cc2bec2ff3cea00584e7175fc4d2468e803ef869
Ankit Aggarwal <ankitaggarwal200515@gmail.com>
2022-12-29 15:54:41 +0530

    CSv to xml Functionality added

--- Demo XML.py
+++ Demo XML.py
@@ -4,6 +4,7 @@ def test_main():
     inp = int(input("""Enter 1 to show all record
 Enter 2 to search for a record by element
 Enter 3 to check if a record is present in the file
+Enter 4 to convert csv to xml file.
 
 Enetr Choice: """))
     if inp == 1:
@@ -12,6 +13,8 @@ Enetr Choice: """))
         element_search()
     elif inp == 3:
         check_element()
+    elif inp == 4:
+        convert_csv_xml() 
 
 
 def open_xml():
@@ -46,6 +49,13 @@ def check_element():
     if not flag:
         print("Element is not present in the XML file")
 
+
+def convert_csv_xml():
+    csv_f = open("Tests\csv 1.csv","r")  #input("Enter CSV File path: ")
+    xml_f = open("Tests\XML 4.xml","w")  #input("Enter XMl file path: ")
+    root_element = "Student_Data"        #input("Enter Root_Element: ")
+    key = "name"                         #input("Enter Key: ")
+    py4xml.csv_to_xml(root_element,csv_f,key,xml_f)
 
 if __name__ == "__main__":
     test_main()
